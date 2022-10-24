fd066f8469489af7e4e7c2e0b89b46abaf329705
Brenton-Fender <105960365+Brenton-Fender@users.noreply.github.com>
2022-10-24 12:34:39 -0400

    Total vote count completed

--- PyPoll.py
+++ PyPoll.py
@@ -1,42 +1,33 @@
-<<<<<<< HEAD
    # Add our dependencies.
 import csv
 import os
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1 Intialize a total vote counter.
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: read and analyze the data here.
     file_reader = csv.reader(election_data)
 
     # Print the header row.
     headers = next(file_reader)
 
-    print(headers)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        
+        # 2. Add to the total vote count.
+        total_votes += 1
 
-=======
-   # Add our dependencies.
-import csv
-import os
-# Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+# 3. Print the total votes.
+print(total_votes)
 
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
 
-    # To do: read and analyze the data here.
-    file_reader = csv.reader(election_data)
 
-    # Print the header row.
-    headers = next(file_reader)
 
-    print(headers)
-
->>>>>>> 19f3a080745038027cf88170c97b35e33c41c7d9
     
