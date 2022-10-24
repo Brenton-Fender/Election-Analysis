2a311689fc9a4433ef59f521ae059ce926f67e23
Brenton-Fender <105960365+Brenton-Fender@users.noreply.github.com>
2022-10-24 13:21:39 -0400

    Candidate vote counter functioning

--- PyPoll.py
+++ PyPoll.py
@@ -7,9 +7,12 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# 1 Intialize a total vote counter and candidate list.
+# Intialize a total vote counter and candidate list.
 total_votes = 0
+# Declare a list of candidates
 candidate_options = []
+# Declare a dictionary of candidate votes
+candidate_votes = {}
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -22,7 +25,7 @@ with open(file_to_load) as election_data:
     # Print each row in the CSV file.
     for row in file_reader:
         
-        # 2. Add to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
 
         # Print the candidate name from each row
@@ -32,8 +35,14 @@ with open(file_to_load) as election_data:
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
 
-#print the candidate list.
-print(candidate_options)
+            # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+#print the candidate votes.
+print(candidate_votes)
 
 
 
